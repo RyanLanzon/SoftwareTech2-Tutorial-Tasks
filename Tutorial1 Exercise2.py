e55364ef8ac54e64ddf419116a3503b9b9d8617f
RyanLanzon <u3298961@uni.canberra.edu.au>
2026-03-04 14:40:46 +1100

    Add files via upload

---
+++ Tutorial1 Exercise2.py
@@ -0,0 +1,8 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS C:\Users\Ryano> python
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
