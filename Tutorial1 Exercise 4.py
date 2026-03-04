70007022fc144e7d31d9021a57d37af9e3c14c8d
RyanLanzon <u3298961@uni.canberra.edu.au>
2026-03-04 14:45:08 +1100

    Add files via upload

---
+++ Tutorial1 Exercise 4.py
@@ -0,0 +1,17 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS C:\Users\Ryano> cd Exercise3
+PS C:\Users\Ryano\Exercise3> notepad first.py
+PS C:\Users\Ryano\Exercise3> python first.py
+PS C:\Users\Ryano\Exercise3> python first.py
+PS C:\Users\Ryano\Exercise3> python first.py
+a = 10
+b = 5
+Sum: 15
+Product: 50
+Difference: 5
+Division: 2.0
+PS C:\Users\Ryano\Exercise3>
